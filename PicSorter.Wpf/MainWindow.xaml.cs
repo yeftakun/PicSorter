13d58b3e6d400ea2233be4c821f9298ea1690133
yeftakun <yeftakun34@gmail.com>
2026-08-12 14:36:45 +0800

    Statistik Sesi + Shortcut Navigasi

--- PicSorter.Wpf/MainWindow.xaml.cs
+++ PicSorter.Wpf/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ namespace PicSorter.Wpf
     public partial class MainWindow : Window
     {
         private readonly MainViewModel _viewModel;
+        private Point _origin;
+        private Point _start;
 
         public MainWindow()
         {
@@ -47,6 +49,8 @@ namespace PicSorter.Wpf
 
         private void UpdatePreviewImage()
         {
+            ResetZoomAndPan();
+
             var bytes = _viewModel.CurrentImageBytes;
             if (bytes == null || bytes.Length == 0)
             {
@@ -101,6 +105,71 @@ namespace PicSorter.Wpf
             {
                 await _viewModel.TryGetCommandForKey(keyStr);
                 e.Handled = true;
+            }
+        }
+
+        private void ResetZoomAndPan()
+        {
+            if (imgScale != null)
+            {
+                imgScale.ScaleX = 1.0;
+                imgScale.ScaleY = 1.0;
+            }
+            if (imgTranslate != null)
+            {
+                imgTranslate.X = 0.0;
+                imgTranslate.Y = 0.0;
+            }
+        }
+
+        private void GridPreview_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (imgPreview.Source == null) return;
+
+            var st = imgScale;
+            var tt = imgTranslate;
+
+            double zoom = e.Delta > 0 ? .1 : -.1;
+            if (!(e.Delta > 0) && (st.ScaleX < .2 || st.ScaleY < .2))
+                return;
+
+            Point relative = e.GetPosition(imgPreview);
+            double absoluteX = relative.X * st.ScaleX + tt.X;
+            double absoluteY = relative.Y * st.ScaleY + tt.Y;
+
+            st.ScaleX += zoom;
+            st.ScaleY += zoom;
+
+            tt.X = absoluteX - relative.X * st.ScaleX;
+            tt.Y = absoluteY - relative.Y * st.ScaleY;
+        }
+
+        private void GridPreview_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (imgPreview.Source == null) return;
+
+            var tt = imgTranslate;
+            _start = e.GetPosition(gridPreview);
+            _origin = new Point(tt.X, tt.Y);
+            gridPreview.CaptureMouse();
+        }
+
+        private void GridPreview_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (gridPreview.IsMouseCaptured)
+            {
+                gridPreview.ReleaseMouseCapture();
+            }
+        }
+
+        private void GridPreview_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (gridPreview.IsMouseCaptured)
+            {
+                var tt = imgTranslate;
+                Vector v = _start - e.GetPosition(gridPreview);
+                tt.X = _origin.X - v.X;
+                tt.Y = _origin.Y - v.Y;
             }
         }
     }
